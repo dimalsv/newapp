b85685e228f0f91a1d3cc43c02f2f01579e9cc69
Dima Losev <dimalosev@Dimas-MacBook-Air.local>
2022-01-23 14:12:49 +0200

    first commit

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,23 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = Room.public_only
+    @room = Room.new
+    @users = User.except_me(current_user)
+  end
+
+  def show
+    @rooms = Room.public_only
+    @users = User.except_me(current_user)
+    @room = Room.new
+
+    @single_room = Room.find(params[:id])
+    @message = Message.new
+    @messages = @single_room.messages
+
+    render "index"
+  end
+
+  def create
+    @room = Room.create(name: params["room"]["name"])
+  end
+end
